eda435e48f8fe3dc5d127c8e5fbf04dffac1f2ff
Abbey Salamera <u1310184@gcloud.utah.edu>
2024-04-08 12:38:19 -0600

    mvt

--- final/kdrama/urls.py
+++ final/kdrama/urls.py
@@ -17,4 +17,43 @@ urlpatterns = [
 
     path('actor_list/', views.ActorList.as_view(), name='actor-list'),
     path('actor_details/<int:actor_id>/', views.ActorDetails.as_view(), name='actor_details'),
- 
+    path('actor_add/', views.ActorAdd.as_view(), name='actor_add'),
+    path('actor_update/<int:actor_id>/', views.ActorUpdate.as_view(), name='actor_update'),
+    path('actor_delete/<int:kdrama_id>/', views.ActorDelete.as_view(), name='actor_delete'),
+
+    path('director_list/', views.DirectorList.as_view(), name='director-list'),
+    path('director_details/<int:director_id>/', views.DirectorDetails.as_view(), name='director_details'),
+    path('director_add/', views.DirectorAdd.as_view(), name='director_add'),
+    path('director_update/<int:director_id>/', views.DirectorUpdate.as_view(), name='director_update'),
+    path('director_delete/<int:director_id>/', views.DirectorDelete.as_view(), name='director_delete'),
+
+    path('prodcompany_list/', views.ProdCompanyList.as_view(), name='prodcompany-list'),
+    path('prodcompany_details/<int:prod_company_id>/', views.ProdCompanyDetails.as_view(), name='prodcompany_details'),
+    path('prodcompany_add/', views.ProdCompanyAdd.as_view(), name='prodcompany_add'),
+    path('prodcompany_update/<int:prod_company_id>/', views.ProdCompanyUpdate.as_view(), name='prodcompany_update'),
+    path('prodcompany_delete/<int:prod_company_id>/', views.ProdCompanyDelete.as_view(), name='prodcompany_delete'),
+
+    path('award_list/', views.AwardList.as_view(), name='award-list'),
+    path('award_details/<int:award_id>/', views.AwardDetails.as_view(), name='award_details'),
+    path('award_add/', views.AwardAdd.as_view(), name='award_add'),
+    path('award_update/<int:award_id>/', views.AwardUpdate.as_view(), name='award_update'),
+    path('award_delete/<int:award_id>/', views.AwardDelete.as_view(), name='award_delete'),
+
+    path('kdrama/', views.KdramaListCreateView.as_view(), name = 'kdrama-list-create'),
+    path('kdrama/<int:pk>/', views.KdramaDetailView.as_view(), name='kdrama-detail'),
+
+    path('character/', views.CharacterListCreateView.as_view(), name = 'character-list-create'),
+    path('character/<int:pk>/', views.CharacterDetailView.as_view(), name='character-detail'),
+
+    path('actor/', views.ActorListCreateView.as_view(), name = 'actor-list-create'),
+    path('actor/<int:pk>/', views.ActorDetailView.as_view(), name='actor-detail'),
+
+    path('director/', views.DirectorListCreateView.as_view(), name = 'director-list-create'),
+    path('director/<int:pk>/', views.DirectorDetailView.as_view(), name='director-detail'),
+
+    path('prod_company/', views.ProdCompanyListCreateView.as_view(), name = 'prodcompany-list-create'),
+    path('prod_company/<int:pk>/', views.ProdCompanyDetailView.as_view(), name='prodcompany-detail'),
+
+    path('award/', views.AwardListCreateView.as_view(), name = 'award-list-create'),
+    path('award/<int:pk>/', views.AwardDetailView.as_view(), name='award-detail'),
+]
--- final/kdrama/views.py
+++ final/kdrama/views.py
@@ -58,4 +58,477 @@ class KdramaUpdate(View):
         
         return render(request=request, template_name='kdrama/kdrama_update.html', context={'form': form, 'kdrama': kdrama})
 
-class KdramaDe
+class KdramaDelete(View):
+
+    def get(self,request,kdrama_id=None):
+
+        if kdrama_id:
+            kdrama = kdrama.objects.get(pk=kdrama_id)
+        else:
+            kdrama = kdrama()
+
+        form = KdramaForm(instance=kdrama)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'kdrama/kdrama_delete.html',context = {'kdrama':kdrama,'form':form})
+      
+    
+    def post(self,request,kdrama_id=None):
+
+        kdrama = kdrama.objects.get(pk=kdrama_id)
+
+        form = KdramaForm(request.POST,instance=kdrama)
+
+        kdrama.delete()
+
+        return redirect(reverse("kdrama-list"))
+
+class CharacterList(View):
+    def get(self, request):
+        characters = character.objects.all()
+        return render(request=request, template_name='kdrama/character_list.html', context={'characters': characters})
+
+class CharacterDetails(View):
+    def get(self, request, character_id):
+        character = get_object_or_404(character, pk=character_id)
+        return render(request=request, template_name='kdrama/character_details.html', context={'character': character})
+
+    
+class CharacterAdd(View):
+    def get(self, request):
+
+        form = CharacterForm()
+
+        return render(request=request, template_name='kdrama/character_add.html', context={'form': form})
+
+    def post(self, request):
+
+        form = CharacterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('character-list') 
+        
+        return render(request=request, template_name='kdrama/character_add.html', context={'form': form})
+
+class CharacterUpdate(View):
+
+    def get(self, request, character_id):
+
+        movie = get_object_or_404(character, pk=character_id)
+
+        form = CharacterForm(instance=character)
+
+        return render(request=request, template_name='kdrama/character_update.html', context={'form': form, 'character': character})
+
+    def post(self, request, character_id):
+
+        character = get_object_or_404(character, pk=character_id)
+
+        form = CharacterForm(request.POST, instance=character)
+
+        if form.is_valid():
+            form.save()
+            return redirect('character-list')
+        
+        return render(request=request, template_name='kdrama/character_update.html', context={'form': form, 'character': character})
+
+class CharacterDelete(View):
+
+    def get(self,request,character_id=None):
+
+        if character_id:
+            character = character.objects.get(pk=character_id)
+        else:
+            character = character()
+
+        form = CharacterForm(instance=character)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'kdrama/character_delete.html',context = {'character':character,'form':form})
+      
+    
+    def post(self,request,character_id=None):
+
+        character = character.objects.get(pk=character_id)
+
+        form = CharacterForm(request.POST,instance=character)
+
+        character.delete()
+
+        return redirect(reverse("character-list"))
+
+
+class ActorList(View):
+    def get(self, request):
+        actors = actor.objects.all()
+        return render(request=request, template_name='kdrama/actor_list.html', context={'actors': actors})
+
+class ActorDetails(View):
+    def get(self, request, actor_id):
+        actor = get_object_or_404(actor, pk=actor_id)
+        return render(request=request, template_name='kdrama/actor_details.html', context={'actor': actor})
+
+    
+class ActorAdd(View):
+    def get(self, request):
+
+        form = ActorForm()
+
+        return render(request=request, template_name='kdrama/actor_add.html', context={'form': form})
+
+    def post(self, request):
+
+        form = ActorForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('actor-list') 
+        
+        return render(request=request, template_name='kdrama/actor_add.html', context={'form': form})
+
+class ActorUpdate(View):
+
+    def get(self, request, actor_id):
+
+        actor = get_object_or_404(actor, pk=actor_id)
+
+        form = ActorForm(instance=actor)
+
+        return render(request=request, template_name='kdrama/actor_update.html', context={'form': form, 'actor': actor})
+
+    def post(self, request, actor_id):
+
+        actor = get_object_or_404(actor, pk=actor_id)
+
+        form = ActorForm(request.POST, instance=actor)
+
+        if form.is_valid():
+            form.save()
+            return redirect('actor-list')
+        
+        return render(request=request, template_name='kdrama/actor_update.html', context={'form': form, 'actor': actor})
+
+class ActorDelete(View):
+
+    def get(self,request,actor_id=None):
+
+        if actor_id:
+            actor = actor.objects.get(pk=actor_id)
+        else:
+            actor = actor()
+
+        form = ActorForm(instance=actor)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'kdrama/actor_delete.html',context = {'actor':actor,'form':form})
+      
+    
+    def post(self,request,actor_id=None):
+
+        actor = actor.objects.get(pk=actor_id)
+
+        form = ActorForm(request.POST,instance=actor)
+
+        actor.delete()
+
+        return redirect(reverse("actor-list"))
+    
+class DirectorList(View):
+    def get(self, request):
+        directors = director.objects.all()
+        return render(request=request, template_name='kdrama/director_list.html', context={'directors': directors})
+
+class DirectorDetails(View):
+    def get(self, request, director_id):
+        director = get_object_or_404(actor, pk=director_id)
+        return render(request=request, template_name='kdrama/director_details.html', context={'director': director})
+
+    
+class DirectorAdd(View):
+    def get(self, request):
+
+        form = DirectorForm()
+
+        return render(request=request, template_name='kdrama/director_add.html', context={'form': form})
+
+    def post(self, request):
+
+        form = DirectorForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('director-list') 
+        
+        return render(request=request, template_name='kdrama/director_add.html', context={'form': form})
+
+class DirectorUpdate(View):
+
+    def get(self, request, director_id):
+
+        director = get_object_or_404(director, pk=director_id)
+
+        form = DirectorForm(instance=director)
+
+        return render(request=request, template_name='kdrama/director_update.html', context={'form': form, 'director': director})
+
+    def post(self, request, director_id):
+
+        director = get_object_or_404(director, pk=director_id)
+
+        form = DirectorForm(request.POST, instance=director)
+
+        if form.is_valid():
+            form.save()
+            return redirect('director-list')
+        
+        return render(request=request, template_name='kdrama/director_update.html', context={'form': form, 'director': director})
+
+class DirectorDelete(View):
+
+    def get(self,request,director_id=None):
+
+        if director_id:
+            director = director.objects.get(pk=director_id)
+        else:
+            director = director()
+
+        form = DirectorForm(instance=director)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'kdrama/director_delete.html',context = {'director':director,'form':form})
+      
+    
+    def post(self,request,director_id=None):
+
+        director = director.objects.get(pk=director_id)
+
+        form = DirectorForm(request.POST,instance=director)
+
+        director.delete()
+
+        return redirect(reverse("director-list"))
+
+class ProdCompanyList(View):
+    def get(self, request):
+        prod_companies = prod_company.objects.all()
+        return render(request=request, template_name='kdrama/prodcompany_list.html', context={'prod_companies': prod_companies})
+
+class ProdCompanyDetails(View):
+    def get(self, request, prod_company_id):
+        prod_company = get_object_or_404(actor, pk=prod_company_id)
+        return render(request=request, template_name='kdrama/prodcompany_details.html', context={'prod_company': prod_company})
+
+    
+class ProdCompanyAdd(View):
+    def get(self, request):
+
+        form = ProdCompanyForm()
+
+        return render(request=request, template_name='kdrama/prodcompany_add.html', context={'form': form})
+
+    def post(self, request):
+
+        form = ProdCompanyForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('prodcompany-list') 
+        
+        return render(request=request, template_name='kdrama/prodcompany_add.html', context={'form': form})
+
+class ProdCompanyUpdate(View):
+
+    def get(self, request, prod_company_id):
+
+        prod_company = get_object_or_404(prod_company, pk=prod_company_id)
+
+        form = ProdCompanyForm(instance=prod_company)
+
+        return render(request=request, template_name='kdrama/prodcompany_update.html', context={'form': form, 'prod_company': prod_company})
+
+    def post(self, request, prod_company_id):
+
+        prod_company = get_object_or_404(prod_company, pk=prod_company_id)
+
+        form = ProdCompanyForm(request.POST, instance=prod_company)
+
+        if form.is_valid():
+            form.save()
+            return redirect('prodcompany-list')
+        
+        return render(request=request, template_name='kdrama/prodcompany_update.html', context={'form': form, 'prod_company': prod_company})
+
+class ProdCompanyDelete(View):
+
+    def get(self,request,prod_company_id=None):
+
+        if prod_company_id:
+            prod_company = prod_company.objects.get(pk=prod_company_id)
+        else:
+            prod_company = prod_company()
+
+        form = ProdCompanyForm(instance=prod_company)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'kdrama/prodcompany_delete.html',context = {'prod_company':prod_company,'form':form})
+      
+    
+    def post(self,request,prod_company_id=None):
+
+        prod_company = prod_company.objects.get(pk=prod_company_id)
+
+        form = ProdCompanyForm(request.POST,instance=prod_company)
+
+        prod_company.delete()
+
+        return redirect(reverse("prodcompany-list"))
+
+class AwardList(View):
+    def get(self, request):
+        awards = award.objects.all()
+        return render(request=request, template_name='kdrama/award_list.html', context={'awards': awards})
+
+class AwardDetails(View):
+    def get(self, request, award_id):
+        award = get_object_or_404(award, pk=award_id)
+        return render(request=request, template_name='kdrama/award_details.html', context={'award': award})
+
+    
+class AwardAdd(View):
+    def get(self, request):
+
+        form = AwardForm()
+
+        return render(request=request, template_name='kdrama/award_add.html', context={'form': form})
+
+    def post(self, request):
+
+        form = AwardForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('award-list') 
+        
+        return render(request=request, template_name='kdrama/award_add.html', context={'form': form})
+
+class AwardUpdate(View):
+
+    def get(self, request, award_id):
+
+        award = get_object_or_404(award, pk=award_id)
+
+        form = AwardForm(instance=award)
+
+        return render(request=request, template_name='kdrama/award_update.html', context={'form': form, 'award': award})
+
+    def post(self, request, award_id):
+
+        kdrama = get_object_or_404(kdrama, pk=award_id)
+
+        form = AwardForm(request.POST, instance=award)
+
+        if form.is_valid():
+            form.save()
+            return redirect('award-list')
+        
+        return render(request=request, template_name='kdrama/award_update.html', context={'form': form, 'award': award})
+
+class AwardDelete(View):
+
+    def get(self,request,award_id=None):
+
+        if award_id:
+            award = award.objects.get(pk=award_id)
+        else:
+            award = award()
+
+        form = AwardForm(instance=award)
+
+        for field in form.fields:
+            form.fields[field].widget.attrs['disabled'] = True
+
+        return render(request = request,template_name = 'kdrama/award_delete.html',context = {'award':award,'form':form})
+      
+    
+    def post(self,request,award_id=None):
+
+        award = award.objects.get(pk=award_id)
+
+        form = AwardForm(request.POST,instance=award)
+
+        award.delete()
+
+        return redirect(reverse("award-list"))
+
+
+class KdramaListCreateView(generics.ListCreateAPIView):
+
+    queryset = kdrama.objects.all()
+    serializer_class = KdramaSerializer
+
+class KdramaDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = kdrama.objects.all()
+    serializer_class = KdramaSerializer
+
+
+class CharacterListCreateView(generics.ListCreateAPIView):
+
+    queryset = character.objects.all()
+    serializer_class = CharacterSerializer
+
+class CharacterDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = character.objects.all()
+    serializer_class = CharacterSerializer
+
+class ActorListCreateView(generics.ListCreateAPIView):
+
+    queryset = actor.objects.all()
+    serializer_class = ActorSerializer
+
+class ActorDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = actor.objects.all()
+    serializer_class = ActorSerializer
+
+class DirectorListCreateView(generics.ListCreateAPIView):
+
+    queryset = director.objects.all()
+    serializer_class = DirectorSerializer
+
+class DirectorDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = director.objects.all()
+    serializer_class = DirectorSerializer
+
+class ProdCompanyListCreateView(generics.ListCreateAPIView):
+
+    queryset = prod_company.objects.all()
+    serializer_class = ProdCompanySerializer
+
+class ProdCompanyDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = prod_company.objects.all()
+    serializer_class = ProdCompanySerializer
+
+class AwardListCreateView(generics.ListCreateAPIView):
+
+    queryset = award.objects.all()
+    serializer_class = AwardSerializer
+
+class AwardDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = award.objects.all()
+    serializer_class = AwardSerializer
