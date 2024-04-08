ae8119a6999cc8da387053b93a6c368a7e478c17
Abbey Salamera <u1310184@gcloud.utah.edu>
2024-04-07 23:20:45 -0600

    test

--- final/kdrama/models.py
+++ final/kdrama/models.py
@@ -7,11 +7,17 @@ class kdrama(models.Model):
     description = models.CharField(max_length=500, blank=True)
     release_year = models.CharField(max_length=50, blank=True)
     duration = models.CharField(max_length=50, blank=True)
+    
+    def __str__(self):
+        return self.title
 
 class character(models.Model):
     character_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, blank=True)
     kdrama = models.ForeignKey(kdrama, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class actor(models.Model):
     actor_id = models.AutoField(primary_key=True)
@@ -20,11 +26,17 @@ class actor(models.Model):
     birth_date = models.DateField(blank=True, null=True)
     character = models.ForeignKey(character, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.first_name, self.last_name
+
 class director (models.Model):
     director_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=25, blank=True)
     last_name = models.CharField(max_length=25, blank=True)
     kdrama = models.ForeignKey(kdrama, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name, self.last_name
 
 class prod_company(models.Model):
     prod_company_id = models.AutoField(primary_key=True)
@@ -32,11 +44,17 @@ class prod_company(models.Model):
     year_founded = models.CharField(max_length=50, blank=True)
     kdrama = models.ForeignKey(kdrama, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 class award(models.Model):
     award_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, blank=True)
     type = models.CharField(max_length=100, blank=True)
     kdrama = models.ForeignKey(kdrama, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
@@ -45,3 +63,6 @@ class User(models.Model):
     first_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
