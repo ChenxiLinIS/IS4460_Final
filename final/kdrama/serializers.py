75331c6cc3b1d2ee06a9a451b5a8fe79d00d1b99
Abbey Salamera <u1310184@gcloud.utah.edu>
2024-04-07 23:44:24 -0600

    serializers

---
+++ final/kdrama/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import kdrama, character, actor, director, prod_company, award
+
+class KdramaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = kdrama
+        fields = '__all__'
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = character
+        fields = '__all__'
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = actor
+        fields = '__all__'
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = director
+        fields = '__all__'
+
+class ProdCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = prod_company
+        fields = '__all__'
+
+class AwardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = award
+        fields = '__all__'
--- final/kdrama/views.py
+++ final/kdrama/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse
+from django.views import View
+from .models import kdrama, character, actor, director, prod_company, award
+from rest_framework import generics
+from .serializers import KdramaSerializer, CharacterSerializer, ActorSerializer, DirectorSerializer, ProdCompanySerializer, AwardSerializer
 
 # Create your views here.
+
+class KdramaListCreateView(generics.ListCreateAPIView):
+
+    queryset = kdrama.objects.all()
+    serializer_class = KdramaSerializer
+
+class KdramaDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = kdrama.objects.all()
+    serializer_class = KdramaSerializer
+
+class CharacterListCreateView(generics.ListCreateAPIView):
+
+    queryset = character.objects.all()
+    serializer_class = CharacterSerializer
+
+class CharacterDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = character.objects.all()
+    serializer_class = CharacterSerializer
+
+class ActorListCreateView(generics.ListCreateAPIView):
+
+    queryset = actor.objects.all()
+    serializer_class = ActorSerializer
+
+class ActorDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = actor.objects.all()
+    serializer_class = ActorSerializer
+
+class DirectorListCreateView(generics.ListCreateAPIView):
+
+    queryset = director.objects.all()
+    serializer_class = DirectorSerializer
+
+class DirectorDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = director.objects.all()
+    serializer_class = DirectorSerializer
+
+class ProdCompanyListCreateView(generics.ListCreateAPIView):
+
+    queryset = prod_company.objects.all()
+    serializer_class = ProdCompanySerializer
+
+class ProdCompanyDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = prod_company.objects.all()
+    serializer_class = ProdCompanySerializer
+
+class AwardListCreateView(generics.ListCreateAPIView):
+
+    queryset = award.objects.all()
+    serializer_class = AwardSerializer
+
+class AwardDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = award.objects.all()
+    serializer_class = AwardSerializer
