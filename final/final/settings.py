67d0ef42c2972a8610a8ad25a77289b543b64282
Fr4d2024 <ramos.f.1023@gmail.com>
2024-04-09 15:05:59 -0600

    Hello!

--- final/final/settings.py
+++ final/final/settings.py
@@ -127,13 +127,13 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-}
+#REST_FRAMEWORK = {
+#    'DEFAULT_AUTHENTICATION_CLASSES': (
+ #       'rest_framework.authentication.TokenAuthentication',
+ #   ),
+#    'DEFAULT_PERMISSION_CLASSES': (
+ #       'rest_framework.permissions.IsAuthenticated',
+ #   ),
+#}
