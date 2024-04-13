7dcb5c68c630a34a140ad734e9ec7babfcee8661
ChenxiLInIS <chenxilin2000@gmail.com>
2024-04-13 12:22:46 -0600

    hi

--- final/kdrama/admin.py
+++ final/kdrama/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import kdrama,actor,prod_company,award,director
+
+class KdramaAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(kdrama)
+
+
+class actorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(actor)
+
+class prod_companyAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(prod_company)
+
+class awardAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(award)
+
+class directorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(director)
+
+
+
 
 # Register your models here.
