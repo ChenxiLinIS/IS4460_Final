d9b690acc82e84f399457d78833d1fc555a063f7
ChenxiLInIS <chenxilin2000@gmail.com>
2024-04-13 14:01:31 -0600

    hi

--- final/final/urls.py
+++ final/final/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("kdrama/", include("kdrama.urls")),
-    path("accounts/",include("django.contrib.auth.urls")),,
+    path("accounts/",include("django.contrib.auth.urls")),
 ]
